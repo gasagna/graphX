6b4d18607c94e5dad6bc44d0de996e843fd9d96e
Davide Lasagna <lasagnadavide@gmail.com>
2011-12-05 23:40:55 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='graphX',
+      version='0.1',
+      description='A utility to extract data from graphics',
+      author='Davide Lasagna',
+      author_email='lasagnadavide@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      scripts = ['graphX']
+     ) 

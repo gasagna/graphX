9588c111cf605e53604fc6e66d41f7fa246f786f
Davide Lasagna <lasagnadavide@gmail.com>
2011-12-12 22:51:55 +0100

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='graphX',
-      version='0.1.1',
+      version='0.1.2',
       description='A utility to extract data from graphics',
       author='Davide Lasagna',
       author_email='lasagnadavide@gmail.com',
